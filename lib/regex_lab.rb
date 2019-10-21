d949f219ab30f078ec3e3d7815b7d97e75679cec
Wendy Harris <wendyv510@hotmail.com>
2019-10-21 17:50:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,12 +7,11 @@ end
 starts_with_a_vowel?("afoot", "excellent", "incredible", "absolute", "unrealy", "inconcievable", "octopus", "chandoo", "crenel", "crooked", "flimsy", "folksy", "dog", "hermit", "makeshift", "Palazzi", "sixfold", "Truth")
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text = "unassuming, ambiguous, understanding, pomp, circumstance, uninteresting, uncompromising, grouchy, corollary"
    
    text.scan (\/w+un..ing/) 
 end
 
-words_starting_with_un_and_ending_with_ing()
+words_starting_with_un_and_ending_with_ing("unassuming, ambiguous, understanding, pomp, circumstance, uninteresting, uncompromising, grouchy, corollary")
 
 def words_five_letters_long(text)
   text = "extreme, briny, crepe, parking, snaps, grouping, snafu, round, dog, be, fork, spoon"
@@ -20,6 +19,8 @@ def words_five_letters_long(text)
   text.grep(/^\w{5}\s/)
 
 end
+
+words_five_letter_long(
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 

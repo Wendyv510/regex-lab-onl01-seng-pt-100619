a2b4d5e015b115c275d1b8d4618ed7437eef69b7
Wendy Harris <wendyv510@hotmail.com>
2019-10-21 17:55:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,17 +14,26 @@ end
 words_starting_with_un_and_ending_with_ing("unassuming, ambiguous, understanding, pomp, circumstance, uninteresting, uncompromising, grouchy, corollary")
 
 def words_five_letters_long(text)
-  text = "extreme, briny, crepe, parking, snaps, grouping, snafu, round, dog, be, fork, spoon"
   
   text.grep(/^\w{5}\s/)
 
 end
 
-words_five_letter_long(
+words_five_letter_long("extreme, briny, crepe, parking, snaps, grouping, snafu, round, dog, be, fork, spoon")
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
 end
+
+first_word_capitalized_and_ends_with_punctuation("I stepped on a Corn Flake, now I'm a cereal killer.")
+
+first_word_capitalized_and_ends_with_punctuation("i wondered why the baseball was getting bigger. Then it hit me." )
+
+first_word_capitalized_and_ends_with_punctuation("Did you hear about the guy whose whole left side was cut off? He's a right now")
+
+first_word_capitalized_and_ends_with_punctuation("when fish are in schools, they sometimes take debate")
+
+
 
 def valid_phone_number?(phone)
 

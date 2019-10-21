012a2e287a60ef8f956adc440ea1fe6c760360af
Wendy Harris <wendyv510@hotmail.com>
2019-10-21 17:49:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,18 @@
 def starts_with_a_vowel?(word)
-  word = ["afoot", "excellent", "incredible", "absolute", "unrealy", "inconcievable", "octopus", "chandoo", "crenel", "crooked", "flimsy", "folksy", "dog", "hermit", "makeshift", "Palazzi", "sixfold", "Truth"]
   
   word.scan (\start_with/[aeiou]/)
 
 end
 
-starts_with_a_vowel?()
+starts_with_a_vowel?("afoot", "excellent", "incredible", "absolute", "unrealy", "inconcievable", "octopus", "chandoo", "crenel", "crooked", "flimsy", "folksy", "dog", "hermit", "makeshift", "Palazzi", "sixfold", "Truth")
 
 def words_starting_with_un_and_ending_with_ing(text)
    text = "unassuming, ambiguous, understanding, pomp, circumstance, uninteresting, uncompromising, grouchy, corollary"
    
    text.scan (\/w+un..ing/) 
 end
+
+words_starting_with_un_and_ending_with_ing()
 
 def words_five_letters_long(text)
   text = "extreme, briny, crepe, parking, snaps, grouping, snafu, round, dog, be, fork, spoon"
